43f2f29c1b55df59e6e127417498c676d28950dc
lpcyber1 <liam.paul@mymail.champlain.edu>
2024-12-03 13:42:54 -0500

    Added Linked Clone Script

---
+++ Clone-VM.ps1
@@ -0,0 +1,32 @@
+# Script written by Liam Paul for cloning a VM using the HyperV Powershell Module
+
+#Writes out names of Base VMs to clone
+$Available = Get-VM | Where-Object {$_.Name -match "Base"}
+
+Write-Host $Available
+
+#Prompt for name of VM to clone
+$SourceVM = Read-Host "Enter the name of the VM you would like to clone"
+
+#Error handling
+if ($null -eq $SourceVM) {
+    Write-Host "VM '$SourceVM' not found, exiting..."
+    exit
+}
+
+#Prompt for new VM name
+$NewName = Read-Host "Enter the name of the new VM"
+
+#Gets the parent disk of the VM that was entered to be cloned
+$ParentPath = "C:\ProgramData\Microsoft\Windows\Virtual Hard Disks\$SourceVM.vhdx"
+
+#Assigns the path and name of the new VM hard disk
+$NewDiskPath = "C:\ProgramData\Microsoft\Windows\Virtual Hard Disks\$NewName.vhdx"
+
+#Creates new differencing disk and gives it 5 seconds to be created before creating the new VM
+$NewDisk = New-VHD -ParentPath $ParentPath -Path $NewDiskPath -Differencing
+Write-Host "Creating new differencing disk..."
+Start-Sleep -Seconds 5
+
+#Creates the Linked Clone using the differencing disk 
+New-VM -Name $NewName -MemoryStartupBytes 2GB -VHDPath $NewDiskPath -Generation 2 
